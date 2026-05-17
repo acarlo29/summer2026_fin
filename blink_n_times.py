429d5668d83549ce544e64c8867d9bd252573836
acarlo29 <a.carlo@wustl.edu>
2026-05-17 13:56:55 -0500

    done with for loop blink n times

--- blink_n_times.py
+++ blink_n_times.py
@@ -11,20 +11,10 @@ parser.add_argument("--num", type=int, default=5, help="Number of times to blink
 args = parser.parse_args()
 
 pin1 = 11
-
 GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)
-
-# ITER_COUNT = args.num  
-
+ 
 print(f"Blinking {args.num} times...")
-sleep(0.5)
-
-# while ITER_COUNT > 0:
-#    ITER_COUNT -= 1
-#    GPIO.output(pin1, GPIO.HIGH)
-#    sleep(1)
-#    GPIO.output(pin1, GPIO.LOW)
-#    sleep(1)
+sleep(1)
 
 for i in range(args.num):
     GPIO.output(pin1, GPIO.HIGH)
