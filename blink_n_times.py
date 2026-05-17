c3591e4ec7c13f47b6f86ceba1f02a1a01e4f209
acarlo29 <a.carlo@wustl.edu>
2026-05-17 13:49:58 -0500

    old

--- blink_n_times.py
+++ blink_n_times.py
@@ -1,17 +1,25 @@
 import sys  
-import RPi.GPIO as GPIO
-from time import sleep
+import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library 
+from time import sleep     # Import the sleep from time module 
+GPIO.setwarnings(False)    # Ignore warning for now 
+GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
 
-pin1 = 11
+pin1 = 11 
 
-# Get blink count from user
-try:
-    num_blinks = int(input("How many times should the LED blink? "))
-    if num_blinks <= 0:
-        print("Please enter a positive number. Defaulting to 5.")
-        num_blinks = 5
-except ValueError:
-    print("Invalid input. Defaulting to 5 blinks.")
+if len(sys.argv) == 1:
+   ITER_COUNT = 5
+else:
+   num_blinks = sys.argv[1]  
+   ITER_COUNT = int(num_blinks) 
+
+GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)    
+
+while ITER_COUNT > 0: # Run ITER_COUNT times 
+   ITER_COUNT -= 1 # Decrement counter 
+   GPIO.output(pin1, GPIO.HIGH) # Turn on 
+   sleep(1)                     # Sleep for 1 second 
+   GPIO.output(pin1, GPIO.LOW)  # Turn off 
+   sleep(1)                     # Sleep for 1 second 
+
+GPIO.cleanup() 
