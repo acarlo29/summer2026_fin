d9de9b4af85a1c0ceace6f96e6fd8b57d66c3753
acarlo29 <a.carlo@wustl.edu>
2026-05-18 19:26:55 -0500

    lgiht rtest

--- blinking_light.py
+++ blinking_light.py
@@ -1,3 +1,17 @@
 # comment for code here
 # please work
 
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(40, GPIO.OUT)
+
+print("LED ON")
+GPIO.output(40, True)
+time.sleep(3)
+
+print("LED OFF")
+GPIO.output(40, False)
+
+GPIO.cleanup()
