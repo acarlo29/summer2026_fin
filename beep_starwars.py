7d3f4eca2b40c25f474e0cbfd4d812aaf40e68c1
acarlo29 <a.carlo@wustl.edu>
2026-05-21 14:25:35 -0500

    code

--- beep_starwars.py
+++ beep_starwars.py
@@ -46,9 +46,9 @@ H = Q * 2           # half note
 
 def play_imperial_march():
     # Phrase 1
-    play_note(A4, DQ); play_note(A4, DQ)
-    play_note(A4, S); play_note(A4, S); play_note(A4, S); play_note(A4, S)
-    play_note(F4, E); play_note(REST, E)
+    # play_note(A4, DQ); play_note(A4, DQ)
+    # play_note(A4, S); play_note(A4, S); play_note(A4, S); play_note(A4, S)
+    # play_note(F4, E); play_note(REST, E)
 
     play_note(A4, DQ); play_note(A4, DQ)
     play_note(A4, S); play_note(A4, S); play_note(A4, S); play_note(A4, S)
@@ -88,7 +88,7 @@ def play_imperial_march():
     play_note(GS4, Q); play_note(F4, DE); play_note(A4, S)
     play_note(A4, Q); play_note(F4, DE); play_note(C5, S); play_note(A4, H)
 
-for i in range(3):
+for i in range(1):
     play_imperial_march()
     time.sleep(0.5)
 
