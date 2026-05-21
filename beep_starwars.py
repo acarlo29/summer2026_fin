62c7704883f1821e2b4eeee89e4ac71b92ee6a59
acarlo29 <a.carlo@wustl.edu>
2026-05-21 14:20:27 -0500

    need to redo code but an idea

--- beep_starwars.py
+++ beep_starwars.py
@@ -16,10 +16,6 @@ def play_note(freq, duration):
     pwm.ChangeDutyCycle(0)
     time.sleep(duration * 0.2)
 
-def rest(duration):
-    pwm.ChangeDutyCycle(0)
-    time.sleep(duration)
-
 # Imperial March
 a  = 440
 f  = 349
@@ -28,28 +24,59 @@ eH = 659
 gH = 784
 fH = 698
 gS = 415
+dH = 587
+aH = 880
 
-play_note(a, 0.5)
-play_note(a, 0.5)
-play_note(a, 0.5)
-play_note(f, 0.35)
-play_note(cH, 0.15)
+def play_imperial_march():
+    # Phrase 1
+    play_note(a, 0.5)
+    play_note(a, 0.5)
+    play_note(a, 0.5)
+    play_note(f, 0.35)
+    play_note(cH, 0.15)
 
-play_note(a, 0.5)
-play_note(f, 0.35)
-play_note(cH, 0.15)
-play_note(a, 1.0)
+    play_note(a, 0.5)
+    play_note(f, 0.35)
+    play_note(cH, 0.15)
+    play_note(a, 1.0)
 
-play_note(eH, 0.5)
-play_note(eH, 0.5)
-play_note(eH, 0.5)
-play_note(fH, 0.35)
-play_note(cH, 0.15)
+    # Phrase 2
+    play_note(eH, 0.5)
+    play_note(eH, 0.5)
+    play_note(eH, 0.5)
+    play_note(fH, 0.35)
+    play_note(cH, 0.15)
 
-play_note(gS, 0.5)
-play_note(f, 0.35)
-play_note(cH, 0.15)
-play_note(a, 1.0)
+    play_note(gS, 0.5)
+    play_note(f, 0.35)
+    play_note(cH, 0.15)
+    play_note(a, 1.0)
+
+    # Phrase 3
+    play_note(aH, 0.5)
+    play_note(a, 0.35)
+    play_note(a, 0.15)
+    play_note(aH, 0.5)
+    play_note(gH, 0.35)
+    play_note(fH, 0.15)
+
+    play_note(fH, 0.25)
+    play_note(eH, 0.25)
+    play_note(fH, 0.5)
+
+    play_note(gS, 0.5)
+    play_note(eH, 0.5)
+    play_note(a, 0.35)
+    play_note(cH, 0.15)
+
+    play_note(eH, 0.5)
+    play_note(a, 0.35)
+    play_note(cH, 0.15)
+    play_note(eH, 1.0)
+
+for i in range(3):
+    play_imperial_march()
+    time.sleep(0.5)  # short pause between repeats
 
 pwm.stop()
 time.sleep(0.1)
