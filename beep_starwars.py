18e1cb7d57b9594a92b1a410d1766ac7471562d4
acarlo29 <a.carlo@wustl.edu>
2026-05-21 14:19:17 -0500

    new one

--- beep_starwars.py
+++ beep_starwars.py
@@ -9,22 +9,51 @@ GPIO.setup(buzzer, GPIO.OUT)
 pwm = GPIO.PWM(buzzer, 440)
 pwm.start(50)
 
-melody = [
-    392, 392, 392,
-    311, 466, 392,
-    311, 466, 392
-]
+def play_note(freq, duration):
+    pwm.ChangeFrequency(freq)
+    pwm.ChangeDutyCycle(50)
+    time.sleep(duration * 0.8)
+    pwm.ChangeDutyCycle(0)
+    time.sleep(duration * 0.2)
 
-durations = [
-    0.4, 0.4, 0.4,
-    0.3, 0.7, 0.4,
-    0.3, 0.7, 0.6
-]
+def rest(duration):
+    pwm.ChangeDutyCycle(0)
+    time.sleep(duration)
 
-for note, dur in zip(melody, durations):
-    pwm.ChangeFrequency(note)
-    time.sleep(dur)
+# Imperial March
+a  = 440
+f  = 349
+cH = 523
+eH = 659
+gH = 784
+fH = 698
+gS = 415
+
+play_note(a, 0.5)
+play_note(a, 0.5)
+play_note(a, 0.5)
+play_note(f, 0.35)
+play_note(cH, 0.15)
+
+play_note(a, 0.5)
+play_note(f, 0.35)
+play_note(cH, 0.15)
+play_note(a, 1.0)
+
+play_note(eH, 0.5)
+play_note(eH, 0.5)
+play_note(eH, 0.5)
+play_note(fH, 0.35)
+play_note(cH, 0.15)
+
+play_note(gS, 0.5)
+play_note(f, 0.35)
+play_note(cH, 0.15)
+play_note(a, 1.0)
 
 pwm.stop()
 time.sleep(0.1)
-GPIO.cleanup()
+try:
+    GPIO.cleanup()
+except:
+    pass
